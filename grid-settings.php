24439ff87840444c87cecf3913318302a2562195
yossidavid <yd3646@gmail.com>
2024-12-09 20:16:14 +0200

    New Plugin

---
+++ grid-settings.php
@@ -0,0 +1,59 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+/**
+ * @param \Elementor\Core\Settings\EditorPreferences\Model $preferences The editor preferences model.
+ */
+function add_preferences_controls(\Elementor\Core\Settings\EditorPreferences\Model $preferences)
+{
+
+	$styliner_grid = get_user_meta(get_current_user_id(), 'styliner_grid', true);
+
+	$styliner_grid = $styliner_grid ? 'yes' : 'no';
+	// $grid_color = $grid_color ? $grid_color : 'rgb(255 0 0 / .15)';
+	// $columns_color = $columns_color ? $columns_color : 'rgb(255 0 0 / .04)';
+	// $rows_color = $rows_color ? $rows_color : 'rgb(255 0 0 / .04)';
+
+	$preferences->add_control(
+		'styliner_grid',
+		[
+			'label' => esc_html__('Enable Styliner Grid', 'textdomain'),
+			'type' => \Elementor\Controls_Manager::SWITCHER,
+			'default' => $styliner_grid,
+			'selectors' => [
+				'#styliner-grid-system' => 'display: block !important;'
+			]
+		]
+	);
+
+	// $preferences->add_control(
+	// 	'grid_color',
+	// 	[
+	// 		'label' => esc_html__('Grids Color', 'textdomain'),
+	// 		'type' => \Elementor\Controls_Manager::COLOR,
+	// 		'default' => $grid_color,
+	// 		'frontend_available' => true,
+	// 	]
+	// );
+
+	// $preferences->add_control(
+	// 	'columns_color',
+	// 	[
+	// 		'label' => esc_html__('Columns Color', 'textdomain'),
+	// 		'type' => \Elementor\Controls_Manager::COLOR,
+	// 		'default' => $columns_color,
+	// 		'frontend_available' => true,
+	// 	]
+	// );
+
+	// $preferences->add_control(
+	// 	'rows_color',
+	// 	[
+	// 		'label' => esc_html__('Rows Color', 'textdomain'),
+	// 		'type' => \Elementor\Controls_Manager::COLOR,
+	// 		'default' => $rows_color,
+	// 		'frontend_available' => true,
+	// 	]
+	// );
+}
+add_action('elementor/element/editor-preferences/preferences/before_section_end', 'add_preferences_controls');
---
+++ gridliner.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+/**
+ * Plugin Name: Gridliner
+ * Author: Yossi David
+ * Author URI: https://shos.digital
+ * Version: 1.0.0
+ * Description: Add the Styliner grid system to Elementor editor, and enable it on the Elementor system preferences
+ */
+
+add_filter('plugin_row_meta', function ($links_array, $plugin_file_name, $plugin_data, $status) {
+	if (strpos($plugin_file_name, basename(__FILE__))) {
+
+		// You can still use `array_unshift()` to add links at the beginning.
+		$links_array[] = '<a href="https://schooliner.com/" target="_blank">סקוליינר</a>';
+		$links_array[] = '<a href="https://schooliner.com/links/" target="_blank">הצטרפו לקבוצת הוואטסאפ</a>';
+		$links_array[] = '<a href="https://wa.me/972559983109/" target="_blank">פיתוח תוספים לוורדפרס</a>';
+	}
+
+	return $links_array;
+}, 10, 4);
+
+
+add_action('elementor/preview/enqueue_scripts', function () {
+	$elementor_preferences = get_user_meta(get_current_user_id(), 'elementor_preferences', true);
+	wp_register_script('gridliner', plugins_url('src/index.js', __FILE__));
+	wp_enqueue_script('gridliner');
+});
+
+add_action('elementor/preview/enqueue_styles', function () {
+	wp_register_style('gridliner', plugins_url('src/index.css', __FILE__));
+	wp_enqueue_style('gridliner');
+});
+
+add_action('elementor/editor/init', function () {
+	require_once plugin_dir_path(__FILE__) . '/grid-settings.php';
+});
