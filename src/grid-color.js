eea9659436c41cae7f7d7e387d8ec3efc292458c
yossidavid <yd3646@gmail.com>
2024-12-19 01:25:00 +0200

    new

--- src/grid-color.js
+++ src/grid-color.js
@@ -1,3 +1,17 @@
 window.addEventListener("DOMContentLoaded", () => {
 	console.log(elementor)
+	const preferences = elementor.settings.editorPreferences
+	console.log(preferences)
+	console.log(preferences.getSettings())
+	console.log(preferences.getEditedView())
+	console.log(preferences.getDataToSave())
+	console.log(preferences.on())
+	preferences.on("update", () => console.log(preferences.getSettings()))
+
+	preferences.on("change:styliner_grid", (_, value) => {
+		const canvas = ensureCanvas()
+		canvas.style.display = value === "yes" ? "block" : "none"
+	})
+
+	window.top.addEventListener("elementor/commands/run/after", console.log(e))
 })
