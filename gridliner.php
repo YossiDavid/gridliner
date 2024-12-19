cf0d58fcc49b93522b04052aca7cc39ea6e10438
yossidavid <yd3646@gmail.com>
2024-12-19 16:56:20 +0200

    Version 1.0.2

--- gridliner.php
+++ gridliner.php
@@ -5,13 +5,13 @@ if (!defined('ABSPATH')) exit;
  * Plugin Name: Gridliner
  * Author: Yossi David
  * Author URI: https://shos.digital
- * Version: 1.0.1
+ * Version: 1.0.2
  * Description: Add the Styliner grid system to Elementor editor, and enable it on the Elementor system preferences
  * Text Domain: gridliner
  */
 
-add_filter( 'plugin_row_meta' , function ( $links_array, $plugin_file_name, $plugin_data, $status ) {
-	if ( strpos( $plugin_file_name, basename( __FILE__ ) ) ) {
+add_filter('plugin_row_meta', function ($links_array, $plugin_file_name, $plugin_data, $status) {
+	if (strpos($plugin_file_name, basename(__FILE__))) {
 
 		// You can still use `array_unshift()` to add links at the beginning.
 		$links_array[] = '<a href="https://schooliner.com/" target="_blank">סקוליינר</a>';
@@ -20,35 +20,35 @@ add_filter( 'plugin_row_meta' , function ( $links_array, $plugin_file_name, $plu
 	}
 
 	return $links_array;
-}, 10, 4 );
+}, 10, 4);
 
 // this adds scripts to the preview iframe of the editor
 add_action('elementor/preview/enqueue_scripts', function () {
 	$handle = 'gridliner';
 	// jquery is a needed dependency for elementor events
-	wp_register_script( $handle, plugins_url( 'src/index.js', __FILE__ ), [ 'jquery' ] );
-	wp_enqueue_script( $handle );
+	wp_register_script($handle, plugins_url('src/index.js', __FILE__), ['jquery']);
+	wp_enqueue_script($handle);
 });
 
 // this adds scripts to the editor panel
 add_action('elementor/editor/after_enqueue_scripts', function () {
 	$handle = 'gridliner_panel';
 	// jquery is a needed dependency for elementor events
-	wp_register_script( $handle, plugins_url( 'src/editor.js', __FILE__ ), [ 'jquery' ] );
-	wp_enqueue_script( $handle );
+	wp_register_script($handle, plugins_url('src/editor.js', __FILE__), ['jquery']);
+	wp_enqueue_script($handle);
 });
 
 add_action('elementor/preview/enqueue_styles', function () {
-	wp_register_style( 'gridliner', plugins_url( 'src/index.css', __FILE__ ) );
-	wp_enqueue_style( 'gridliner' );
+	wp_register_style('gridliner', plugins_url('src/index.css', __FILE__));
+	wp_enqueue_style('gridliner');
 });
 
 add_action('elementor/editor/init', function () {
-	require_once plugin_dir_path( __FILE__ ) . '/grid-settings.php';
+	require_once plugin_dir_path(__FILE__) . '/grid-settings.php';
 });
 
 //Update Manager
-require plugin_dir_path( __FILE__ ) . 'plugin-update-checker-master/plugin-update-checker.php';
+require plugin_dir_path(__FILE__) . 'plugin-update-checker-master/plugin-update-checker.php';
 
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
